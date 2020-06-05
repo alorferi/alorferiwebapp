ff01d828185d08f120e6a424f72de1a01b198d31
Babul Mirdha <babumirdha@gmail.com>
2020-06-05 07:27:04 +0600

    Added Auth Improved design using bootstrup 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,52 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@if (Auth::check())
+    <script type="text/javascript">
+        window.location = "{{ route('me.home') }}";//here double curly bracket
+    </script>
+@endif
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+@section('content')
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <div class="row p-2 mx-auto">
 
-            .position-ref {
-                position: relative;
-            }
+        <div class="col-sm-4">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+            <img src="{{ url('/images/slider_imgs/img_02.png') }}" alt="Chicago" width="360px">
+        </div>
 
-            .content {
-                text-align: center;
-            }
+        <div class="col-sm-4 text-center">
 
-            .title {
-                font-size: 84px;
-            }
+            <img src="{{asset('images/defaults/logo_large.png')}}" width="250" alt="Alor Feri logo">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+            <h3 style="color:#0000A0;text-shadow: 1px 1px #ffffff;"><br/> এসো বই পড়ি <br/> আলোকিত সমাজ গড়ি।</h3>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <br/> <br/>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <a href="https://play.google.com/store/apps/details?id=com.provatsoft.alorferi" target="_blank">
+                <img src="{{asset('images/defaults/google_play_get_on.png')}}" width="250" alt="Alor Feri logo">
+            </a>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            <br/>
+            <br/>
+        </div>
+
+
+        <div class="col-sm-4">
+            @include("partials.login_welcome")
+        </div>
+
+    </div>
+
+    <div>
+
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                Alor Feri © 2020
             </div>
         </div>
-    </body>
-</html>
+
+@endsection
