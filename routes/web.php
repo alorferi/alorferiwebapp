221c3b491664e3347d5786b4b0ae73207df1c58a
Babul Mirdha <babumirdha@gmail.com>
2020-06-05 07:45:23 +0600

    Added clear root.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/clear', function() {
+
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    Artisan::call('clear-compiled');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+
+    return "Cleared!";
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
