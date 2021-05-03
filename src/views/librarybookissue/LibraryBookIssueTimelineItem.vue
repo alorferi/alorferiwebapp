10ac379d582ea843bfa9a6e22f2770cea635e894
Babul Mirdha <babumirdha@gmail.com>
2021-05-03 08:55:39 +0600

    improved design

--- src/views/librarybookissue/LibraryBookIssueTimelineItem.vue
+++ src/views/librarybookissue/LibraryBookIssueTimelineItem.vue
@@ -3,25 +3,26 @@
         <div class="card mb-3 p-2">
             <div class="d-flex">
 
-                <div class="p-2" v-if=" isCollected ">{{this.dateshow(item.collected_at)}} </div>
 
-                <div class="p-2" v-else>{{this.dateshow(item.issued_at)}} </div>
 
                 <b-img-lazy
                     :src="this.getApiUrl(item.library_member.user.photo_url)"
-                    class="p-2"
-                   style=" max-width:50px;max-height:50px;"
+                    class="p-2 rounded-circle"
+                   style=" max-width:48px;max-height:48px;"
                 />
 
                <div class="p-2">
                     {{ item.library_member.user.first_name }}
                 </div>
 
+  <div class="p-2" v-if=" isCollected ">{{this.dateshow(item.collected_at)}} </div>
+
+                <div class="p-2" v-else>{{this.dateshow(item.issued_at)}} </div>
 
                 <b-img-lazy
                     :src="this.getApiUrl(item.library_book.book.cover_url)"
                     class="p-2"
-                     style=" max-width:50px;max-height:50px;"
+                     style=" max-width:48px;max-height:48px;"
                 />
 
                <div class="p-2">
