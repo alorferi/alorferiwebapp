321912d86d9e29d0834ebe68ddaac622678a9c53
Babul Mirdha <babumirdha@gmail.com>
2023-02-21 04:20:14 +0600

    Improved design.

--- src/views/library/LibraryAbout.vue
+++ src/views/library/LibraryAbout.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-             <!-- <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Mobile</p>
+    <div class="border-left border-right border-bottom round">
+        <!-- <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Mobile</p>
             <p class="pl-2 text-blue-400">{{ library.mobile }}</p>
             <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Email</p>
             <p class="pl-2 text-blue-400">{{ library.email }}</p>
@@ -9,99 +9,64 @@
             </p>
             <p class="pl-2 text-blue-400">{{ library.founded_at }}</p> -->
 
+        <div class="p-2 m-3 ">
+            <!-- @if(ValidX::canAccessLibrary($library,"library_edit")==null) -->
+            <div class=" text-right">
+                <!-- <a href="#" class="btn btn-primary">Edit</a> -->
+            </div>
 
+            <!-- @endif -->
 
-<div class="p-2 m-3">
-    <!-- @if(ValidX::canAccessLibrary($library,"library_edit")==null) -->
-    <div class=" text-right">
-        <!-- <a href="#" class="btn btn-primary">Edit</a> -->
-    </div>
+            <!-- @if(library.about) -->
+            <div class="form-group" style="text-align: justify">
+                <!-- {{ str_replace("\n","<br/>",library.about) }} -->
 
-    <!-- @endif -->
+                {{ library.about }}
+            </div>
+            <!-- @endif -->
 
-<!-- @if(library.about) -->
-<div class="form-group" style="text-align: justify">
+            <div class="form-group">Email : {{ library.email }}</div>
 
-    <!-- {{ str_replace("\n","<br/>",library.about) }} -->
+            <div class="form-group">
+                Founded At : {{ this.formatDate(library.founded_at) }}
+            </div>
 
-    {{library.about}}
+            <div class="form-group">
+                Govt Reg No : {{ library.govt_reg_no }}
+            </div>
 
-</div>
-<!-- @endif -->
+            <div class="form-group">Phone : {{ library.phone }}</div>
 
+            <div class="form-group">Mobile : {{ library.mobile }}</div>
 
+            <div class="form-group">Post Code : {{ library.post_code }}</div>
 
-<div class="form-group">
+            <div class="form-group">
+                Total Reqular News Papers :
+                {{ library.total_regular_news_papers }}
+            </div>
 
-   Email : {{ library.email }}
+            <div class="form-group">
+                Total Reqular Magazines : {{ library.total_regular_magazines }}
+            </div>
 
-</div>
+            <div class="form-group">
+                Opening Time :
 
+                {{ library.opening_time_at }}
+            </div>
 
+            <div class="form-group">
+                Closing Time :
+                {{ library.closing_time_at }}
+            </div>
 
-    <div class="form-group">
-       Founded At : {{ this.formatDate(library.founded_at) }}
-    </div>
-
-<div class="form-group">
-  Govt Reg No  : {{ library.govt_reg_no }}
-
-</div>
-
-<div class="form-group">
-  Phone  : {{ library.phone }}
-
-</div>
-
-
-
-<div class="form-group">
-  Mobile  : {{ library.mobile }}
-
-</div>
-
-<div class="form-group">
-  Post Code  : {{ library.post_code }}
-
-</div>
-
-
- <div class="form-group">
-   Total Reqular News Papers : {{ library.total_regular_news_papers }}
-
-</div>
-
-<div class="form-group">
-   Total Reqular Magazines : {{ library.total_regular_magazines }}
-</div>
-
-
-<div class="form-group">
- Opening Time   :
-
-    {{ library.opening_time_at }}
-
-</div>
-
-<div class="form-group">
-Closing Time    :
-    {{ library.closing_time_at }}
-</div>
-
-<div class="form-group">
-
-
-      Off Days  {{offDayname}},
-</div>
-
-</div>
-
+            <div class="form-group">Off Days {{ offDayname }},</div>
+        </div>
     </div>
 </template>
 <script>
-
-export default({
-    props:["library"],
-})
+export default {
+    props: ["library"]
+};
 </script>
-
