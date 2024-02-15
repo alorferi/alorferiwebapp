6642051e2e6541d9cee5e538d2e183eda5318180
Babul Mirdha <babumirdha@gmail.com>
2024-02-15 23:51:16 +0600

    fixed guest bug.

--- src/views/navbars/GuestNav.vue
+++ src/views/navbars/GuestNav.vue
@@ -1,61 +1,47 @@
 <template>
 
-    <div>
+    <div class="container-fluid">
 
+        <router-link :to="{ name: 'home' }" class="navbar-brand">
+            <img src="@/assets/images/defaults/alorferi_logo_brand.png" style="width:40px;" alt="Alor Feri logo" />
+        </router-link>
 
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm  bg-white">
-            <div class="container">
-                <div class="container-fluid">
-                    <router-link :to="{ name: 'home' }" class="navbar-brand">
-                        <img src="@/assets/images/defaults/alorferi_logo_brand.png" style="width:40px;"
-                            alt="Alor Feri logo" />
-                    </router-link>
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                    <!-- Toggler/collapsibe Button -->
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav mr-auto">
 
-                    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                        <ul class="navbar-nav mr-auto">
+                <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
+                    <router-link class="nav-link mx-2" :to="{ name: 'home' }">{{ $t('home') }}</router-link>
+                </li>
 
-                            <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
-                                <router-link class="nav-link mx-2"
-                                    :to="{ name: 'home' }">{{ $t('home') }}</router-link>
-                            </li>
-
-                            <!-- <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
+                <!-- <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
                     <router-link class="nav-link" :to="{ name: 'achievements' }">{{ $t('training') }}</router-link>
                 </li> -->
 
-                            <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
-                                <router-link class="nav-link" :to="{ name: 'about' }">{{ $t('about') }}</router-link>
-                            </li>
-                        </ul>
+                <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
+                    <router-link class="nav-link" :to="{ name: 'about' }">{{ $t('about') }}</router-link>
+                </li>
+            </ul>
 
-                        <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav ml-auto">
 
-                            <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
-                                <router-link class="nav-link" :to="{ name: 'login' }">{{ $t('login') }}</router-link>
-                            </li>
-                            <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
-                                <router-link class="nav-link" :to="{ name: 'register' }">
-                                    {{ $t('sign_up') }}</router-link>
-                            </li>
-                            <li class="nav-item">
-                                <LanguageToggleButton />
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
-        </nav>
-
-
-
+                <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
+                    <router-link class="nav-link" :to="{ name: 'login' }">{{ $t('login') }}</router-link>
+                </li>
+                <li class="nav-item" style="margin-left: 5px;margin-right: 5px;">
+                    <router-link class="nav-link" :to="{ name: 'register' }">
+                        {{ $t('sign_up') }}</router-link>
+                </li>
+                <li class="nav-item">
+                    <LanguageToggleButton />
+                </li>
+            </ul>
+        </div>
     </div>
-
 
 </template>
 
